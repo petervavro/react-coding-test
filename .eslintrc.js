de51b7529c5e555de2b16b533ad9232f2f327376
Peter V <peter@vavro.me>
2021-03-27 12:13:32 -0500

    Update CRA

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,8 +6,6 @@ module.exports = {
   extends: [
     "plugin:react/recommended", // Uses the recommended rules from @eslint-plugin-react
     "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-    "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-    "plugin:prettier/recommended" // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
   globals: {
     Atomics: 'readonly',
@@ -24,8 +22,13 @@ module.exports = {
   plugins: [
     'react',
     '@typescript-eslint',
+    "react-hooks"
   ],
   rules: {
+    "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
+    "react-hooks/exhaustive-deps": "warn", // Checks effect dependencies
+    "@typescript-eslint/explicit-module-boundary-types": "off",
     "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/no-non-null-assertion": "off"
   },
 };
