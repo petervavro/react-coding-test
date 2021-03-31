1e192943bae575aa1752da3052af31db85e312e8
Peter V <peter@vavro.me>
2021-03-30 20:19:03 -0500

    Add more text to welcome page

--- src/components/PageWelcome/PageWelcome.tsx
+++ src/components/PageWelcome/PageWelcome.tsx
@@ -4,9 +4,9 @@ import Typography from '@material-ui/core/Typography';
 
 function PageWelcome() {
     return (
-        <PageLayout title={'Welcome in'}>
+        <PageLayout title={'Welcome'}>
             <Typography variant="body1" gutterBottom>
-                Lorem ipsum dolor sit amet, consectetur.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eu dignissim mauris. Ut et quam turpis. Aliquam quis dignissim tellus. Nullam pharetra mi vitae hendrerit faucibus. Curabitur nisl magna, sagittis eu vehicula ac, vestibulum ut nisi. Cras porta ipsum vitae mattis egestas. Phasellus orci velit, facilisis ornare volutpat in, auctor eu lorem. Vivamus ac nulla in neque sollicitudin consectetur. Curabitur malesuada hendrerit odio, non volutpat enim sagittis non. Nunc vel enim odio. Phasellus ipsum libero, tristique at luctus sed, pharetra feugiat felis. Vestibulum gravida urna eget metus accumsan mollis. Etiam at velit nisi. Vestibulum id nibh imperdiet, pulvinar orci gravida, congue nulla. Proin nec risus id dolor ultrices sodales at dapibus sem.
             </Typography>
         </PageLayout>
     );
